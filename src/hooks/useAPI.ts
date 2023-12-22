a66bfa586cf2cdc6b8a0d54bee8e37eabca0febe
jiweihong0 <jiweihong0@gmail.com>
2023-12-22 09:41:55 +0800

    refactor : useApi useColedit

--- src/hooks/useAPI.ts
+++ src/hooks/useAPI.ts
@@ -12,21 +12,21 @@ const getFetch = (url: string, token: string) => {
   });
 };
 
-// const multateFetch = <T>(
-//   url: string,
-//   method: string,
-//   token: string,
-//   body: T
-// ) => {
-//   return fetch(`${APIBaseURL}/api${url}`, {
-//     method: method,
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${token}`,
-//     },
-//     body: JSON.stringify(body),
-//   });
-// };
+const muliteFetch = <T>(
+  url: string,
+  method: string,
+  token: string,
+  body: T
+) => {
+  return fetch(`${APIBaseURL}/api${url}`, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(body),
+  });
+};
 
 export function useToken() {
   const [token, setToken] = useState<string>("");
@@ -71,5 +71,52 @@ export function useGetCollectionList() {
     isLoading,
     isError,
     datasa,
+  };
+}
+
+export function usePostCollectionAdd() {
+  const token = useToken();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleSaveColumn = async (formDatas: any) => {
+    setIsLoading(true);
+    try {
+      const res = await muliteFetch("/coladd", "POST", token, formDatas);
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSaveNotice = async (notices: any) => {
+    setIsLoading(true);
+    try {
+      const res = await muliteFetch("/coladd/notices", "POST", token, notices);
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    isLoading,
+    isError,
+    handleSaveColumn,
+    handleSaveNotice,
   };
 }
--- src/hooks/useColadd.ts
+++ src/hooks/useColadd.ts
@@ -1,148 +1,137 @@
-import { useState, useEffect } from 'react';
-import { DatePickerProps } from 'antd/lib/date-picker';
+import { useState, useEffect } from "react";
+import { DatePickerProps } from "antd/lib/date-picker";
+import { usePostCollectionAdd } from "./useAPI";
 
 interface FormData {
-    roomNumber: string;
-    expenseName: string;
-    expenseAmount: string;
-    paymentMethod: string;
-    note: string;
-    bankName: string;
-    bankAccount: string;
+  roomNumber: string;
+  expenseName: string;
+  expenseAmount: string;
+  paymentMethod: string;
+  note: string;
+  bankName: string;
+  bankAccount: string;
 }
 
 interface NoticeData {
-    visitDate: string;
-    record: string;
-    remindDate: string;
-    remind: string;
+  visitDate: string;
+  record: string;
+  remindDate: string;
+  remind: string;
 }
 
 const useCustomHook = () => {
-    const [notices, setNotices] = useState<NoticeData[]>([
-        {
-            visitDate: '2024-01-01',
-            record: '',
-            remindDate: '2024-01-31',
-            remind: '',
-        },
-    ]);
-    const [formData, setFormData] = useState<FormData>({
-        roomNumber: '',
-        expenseName: '水費',
-        expenseAmount: '',
-        paymentMethod: '現金',
-        note: '',
-        bankName: '',
-        bankAccount: '',
+  const { isLoading, isError, handleSaveColumn, handleSaveNotice } =
+    usePostCollectionAdd();
+  const [notices, setNotices] = useState<NoticeData[]>([
+    {
+      visitDate: "2024-01-01",
+      record: "",
+      remindDate: "2024-01-31",
+      remind: "",
+    },
+  ]);
+  const [formData, setFormData] = useState<FormData>({
+    roomNumber: "",
+    expenseName: "水費",
+    expenseAmount: "",
+    paymentMethod: "現金",
+    note: "",
+    bankName: "",
+    bankAccount: "",
+  });
+
+  const handleChange = (key: keyof FormData, value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [key]: value,
+    }));
+  };
+
+  const handleNoticeChange = (
+    index: number,
+    key: keyof NoticeData,
+    value: string
+  ) => {
+    setNotices((prevNotices) => {
+      const newNotices = [...prevNotices];
+      newNotices[index] = {
+        ...newNotices[index],
+        [key]: value,
+      };
+      return newNotices;
     });
+  };
 
-    const handleChange = (key: keyof FormData, value: string) => {
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [key]: value,
-        }));
-    };
+  const handleDeleteNotice = (index: number) => {
+    setNotices((prevNotices) => {
+      const newNotices = [...prevNotices];
+      newNotices.splice(index, 1);
+      return newNotices;
+    });
+  };
 
-    const handleNoticeChange = (index: number, key: keyof NoticeData, value: string) => {
-        setNotices((prevNotices) => {
-            const newNotices = [...prevNotices];
-            newNotices[index] = {
-                ...newNotices[index],
-                [key]: value,
-            };
-            return newNotices;
-        });
-    };
+  const handleSave = async () => {
+    await handleSaveColumn(formData);
+    await handleSaveNotice(notices);
+    alert("儲存成功");
+  };
 
-    const handleDeleteNotice = (index: number) => {
-        setNotices((prevNotices) => {
-            const newNotices = [...prevNotices];
-            newNotices.splice(index, 1);
-            return newNotices;
-        });
-    }
+  const handleReset = () => {
+    setFormData({
+      roomNumber: "",
+      expenseName: "水費",
+      expenseAmount: "",
+      paymentMethod: "現金",
+      note: "",
+      bankName: "",
+      bankAccount: "",
+    });
+    setNotices([
+      {
+        visitDate: "2024-01-01",
+        record: "",
+        remindDate: "2024-01-31",
+        remind: "",
+      },
+    ]);
+  };
 
+  const handleAddNotice = () => {
+    setNotices((prevNotices) => {
+      const newNotices = [...prevNotices];
+      newNotices.push({
+        visitDate: "2024-01-01",
+        record: "",
+        remindDate: "2024-01-31",
+        remind: "",
+      });
+      return newNotices;
+    });
+  };
 
-    const handleSave = async () => {
-        const res = await fetch('/api/coledit', {
-            method: 'POST',
-            body: JSON.stringify({
-                formData,
-                
-            }),
-        });
-        if (!res.ok) {
-            throw new Error(res.statusText);
-          }
-        const notices_psot = await fetch('/api/coledit/notices', {
-            method: 'POST',
-            body: JSON.stringify({
-                notices,
-            }),
-        });
-        if (!notices_psot.ok) {
-            throw new Error(notices_psot.statusText);
-          }
-        const data_notice = await notices_psot.json();
-        const data = await res.json();
-        console.log(data);
-        console.log(data_notice);
-    }
+  const onChangeDate: DatePickerProps["onChange"] = (date, dateString) => {
+    handleNoticeChange(0, "visitDate", dateString);
+  };
 
-    const handleReset = () => {
-        setFormData({
-            roomNumber: '',
-            expenseName: '水費',
-            expenseAmount: '',
-            paymentMethod: '現金',
-            note: '',
-            bankName: '',
-            bankAccount: '',
-        });
-        setNotices([
-            {
-                visitDate: '2024-01-01',
-                record: '',
-                remindDate: '2024-01-31',
-                remind: '',
-            },
-        ]);
-    };
-    
-    const handleAddNotice = () => {
-        setNotices((prevNotices) => {
-            const newNotices = [...prevNotices];
-            newNotices.push({
-                visitDate: '2024-01-01',
-                record: '',
-                remindDate: '2024-01-31',
-                remind: '',
-            });
-            return newNotices;
-        });
-    }
+  const onChangeRemindDate: DatePickerProps["onChange"] = (
+    date,
+    dateString
+  ) => {
+    handleNoticeChange(0, "remindDate", dateString);
+  };
 
-    const onChangeDate: DatePickerProps['onChange'] = (date, dateString) => {
-        handleNoticeChange(0, 'visitDate', dateString);
-    };
-
-    const onChangeRemindDate: DatePickerProps['onChange'] = (date, dateString) => {
-        handleNoticeChange(0, 'remindDate', dateString);
-    };
-
-    return {
-        formData,
-        notices,
-        handleChange,
-        handleNoticeChange,
-        handleSave,
-        handleReset,
-        onChangeDate,
-        onChangeRemindDate,
-        handleAddNotice,
-        handleDeleteNotice
-    };
+  return {
+    formData,
+    notices,
+    handleChange,
+    handleNoticeChange,
+    handleSave,
+    handleReset,
+    onChangeDate,
+    onChangeRemindDate,
+    handleAddNotice,
+    handleDeleteNotice,
+  };
 };
 
 export default useCustomHook;
