bed601d2d46cb9a28515eb62b520c192947aad5f
jiweihong0 <jiweihong0@gmail.com>
2023-12-22 14:51:36 +0800

    fix: fix path

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,17 @@
-import Menu from './Menu'
-import { Outlet } from 'react-router-dom'
-export default function Layout(){
-    return(
- 
-        <div className='flex h-full min-h-screen bg-gray-300 '>
-            <Menu/>
-            <Outlet/>
-        </div>
-   
-    )
+import { useEffect } from "react";
+import Menu from "./Menu";
+import { Outlet, useNavigate } from "react-router-dom";
+export default function Layout() {
+  const route = useNavigate();
+
+  useEffect(() => {
+    route("/Collection/List");
+  }, []);
+
+  return (
+    <div className="flex h-full min-h-screen bg-gray-300 ">
+      <Menu />
+      <Outlet />
+    </div>
+  );
 }
