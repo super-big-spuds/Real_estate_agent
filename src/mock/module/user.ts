da8e2f1d3b0500568d4c4fb5039d452395bf6d43
jiweihong0 <jiweihong0@gmail.com>
2023-12-25 17:26:04 +0800

    fix: key remove

--- src/mock/module/user.ts
+++ src/mock/module/user.ts
@@ -8,28 +8,24 @@ export default [
       message: "Successfully get the media",
       data: [
         {
-          key: "1",
           name: "王小明",
           email: "User1@gmail.com",
           isactive: "是",
           id: "1",
         },
         {
-          key: "2",
           name: "王大明",
           email: "User2@gmail.com",
           isactive: "是",
           id: "2",
         },
         {
-          key: "3",
           name: "王中明",
           email: "User3@gmail.com",
           isactive: "否",
           id: "3",
         },
         {
-          key: "4",
           name: "王營明",
           email: "User4@gmail.com",
           isactive: "是",
@@ -65,7 +61,6 @@ export default [
     response: {
       message: "Successfully get the media",
       data: {
-        key: "1",
         name: "王小明",
         email: "User1@gmail.com",
         isactive: "是",
@@ -81,7 +76,6 @@ export default [
     response: {
       message: "Successfully get the media",
       data: {
-        key: "2",
         name: "王大明",
         email: "User2@gmail.com",
         isactive: "是",
@@ -97,7 +91,6 @@ export default [
     response: {
       message: "Successfully get the media",
       data: {
-        key: "3",
         name: "王中明",
         email: "User3@gmail.com",
         password: "123456",
@@ -113,7 +106,6 @@ export default [
     response: {
       message: "Successfully get the media",
       data: {
-        key: "4",
         name: "王營明",
         email: "User4@gmail.com",
         password: "123456",
