bc288e3e7deb0c802788aeeb64367419a369659d
jiweihong0 <jiweihong0@gmail.com>
2023-12-27 11:32:56 +0800

    TMP : tenement page

--- src/components/TenementRent.tsx
+++ src/components/TenementRent.tsx
@@ -45,18 +45,20 @@ export default function CollectionMange(props: any) {
         <button className="flex w-12 h-20" onClick={handleback}>
           {"< 返回"}
         </button>
+        <div className="inline-flex flex-col ml-5">
+          <p className="text-4xl font-bold whitespace-normal">租房資訊</p>
+        </div>
         {isLoading ? (
           <p>loading...</p>
         ) : isError ? (
           <p>error...</p>
         ) : (
-          <div>
-            <div className="inline-flex flex-col ml-5">
-              <p className="text-4xl font-bold whitespace-normal">租房資訊</p>
-            </div>
-            <div className="flex flex-col flex-wrap w-full h-full gap-10 p-10 overflow-visible ">
+          <div className="flex flex-row ">
+            <div className="flex flex-col flex-wrap w-1/2 h-full gap-10 px-16 overflow-visible ">
               <div className="inline-flex items-center whitespace-nowrap w-96">
-                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </p>
                 <p>房號:</p>
                 <InputWithErrorMessage
                   value={formData.tenement_no}
@@ -67,8 +69,11 @@ export default function CollectionMange(props: any) {
               </div>
 
               {/* 面向 */}
-              <div className="inline-flex items-center whitespace-nowrap w-96">
-                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+              <div className="inline-flex items-center w-48 whitespace-nowrap">
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp; &nbsp;
+                </p>
                 <p>面向:</p>
                 <Radio.Group>
                   <Radio value="海景">海景</Radio>
@@ -79,9 +84,31 @@ export default function CollectionMange(props: any) {
                   <Radio value="海景風洞">海景風洞</Radio>
                 </Radio.Group>
               </div>
+              {/* 案件狀態 */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <p>案件狀態:</p>
+                <Radio.Group>
+                  <Radio value="已出租">已出租</Radio>
+                  <Radio value="未出租">未出租</Radio>
+                </Radio.Group>
+              </div>
+              {/* 案件型態 */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <p>案件型態:</p>
+                <Radio.Group>
+                  <Radio value="出租">出租</Radio>
+                  <Radio value="出售">出售</Radio>
+                  <Radio value="開發追蹤">開發追蹤</Radio>
+                  <Radio value="行銷追蹤">行銷追蹤</Radio>
+                </Radio.Group>
+              </div>
               {/* 總坪數 */}
               <div className="inline-flex items-center whitespace-nowrap w-96">
-                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </p>
                 <p>總坪數:</p>
                 <InputWithErrorMessage
                   value={formData.tenement_no}
@@ -92,6 +119,7 @@ export default function CollectionMange(props: any) {
               </div>
               {/* 主建物坪數 */}
               <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>&nbsp;&nbsp;&nbsp;</p>
                 <p>主建物坪數:</p>
                 <InputWithErrorMessage
                   value={formData.tenement_no}
@@ -122,6 +150,9 @@ export default function CollectionMange(props: any) {
               </div>
               {/* 管理費 */}
               <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </p>
                 <p>管理費:</p>
                 <InputWithErrorMessage
                   value={formData.tenement_no}
@@ -130,9 +161,12 @@ export default function CollectionMange(props: any) {
                   errorMessage={"至少兩個字"}
                 />
               </div>
-              {/* 售價 */}
+              {/* 租金 */}
               <div className="inline-flex items-center whitespace-nowrap w-96">
-                <p>售價:</p>
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </p>
+                <p>租金:</p>
                 <InputWithErrorMessage
                   value={formData.tenement_no}
                   onChange={(e) => handleChange("tenement_no", e.target.value)}
@@ -140,7 +174,98 @@ export default function CollectionMange(props: any) {
                   errorMessage={"至少兩個字"}
                 />
               </div>
-              {/* 售價 */}
+              {/* 押金 */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </p>
+                <p>押金:</p>
+                <InputWithErrorMessage
+                  value={formData.tenement_no}
+                  onChange={(e) => handleChange("tenement_no", e.target.value)}
+                  isError={formData.tenement_no.length <= 2}
+                  errorMessage={"至少兩個字"}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col w-1/2 h-full ">
+              {/* 房屋照片 */}
+              <div className="inline-flex flex-col ml-5">
+                <p className="mt-2 text-3xl font-bold whitespace-normal">
+                  房屋照片
+                </p>
+              </div>
+              {/* 屋主姓名 */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <p>屋主姓名:</p>
+                <InputWithErrorMessage
+                  value={formData.collection_name}
+                  onChange={(e) =>
+                    handleChange("collection_name", e.target.value)
+                  }
+                  isError={formData.collection_name.length <= 2}
+                  errorMessage={"至少兩個字"}
+                />
+              </div>
+              {/* 行動電話 */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <p>行動電話:</p>
+                <InputWithErrorMessage
+                  value={formData.collection_name}
+                  onChange={(e) =>
+                    handleChange("collection_name", e.target.value)
+                  }
+                  isError={formData.collection_name.length <= 2}
+                  errorMessage={"至少兩個字"}
+                />
+              </div>
+              {/* 電話 */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp;
+                </p>
+                <p>電話:</p>
+                <InputWithErrorMessage
+                  value={formData.collection_name}
+                  onChange={(e) =>
+                    handleChange("collection_name", e.target.value)
+                  }
+                  isError={formData.collection_name.length <= 2}
+                  errorMessage={"至少兩個字"}
+                />
+              </div>
+              {/* Line */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp;&nbsp; &nbsp;
+                </p>
+                <p>Line:</p>
+                <InputWithErrorMessage
+                  value={formData.collection_name}
+                  onChange={(e) =>
+                    handleChange("collection_name", e.target.value)
+                  }
+                  isError={formData.collection_name.length <= 2}
+                  errorMessage={"至少兩個字"}
+                />
+              </div>
+              {/* 屋主匯款資訊 */}
+              <div className="inline-flex items-center whitespace-nowrap w-96">
+                <p>&nbsp;&nbsp;</p>
+                <p>屋主匯款資訊:</p>
+                <InputWithErrorMessage
+                  value={formData.collection_name}
+                  onChange={(e) =>
+                    handleChange("collection_name", e.target.value)
+                  }
+                  isError={formData.collection_name.length <= 2}
+                  errorMessage={"至少兩個字"}
+                />
+              </div>
             </div>
           </div>
         )}
