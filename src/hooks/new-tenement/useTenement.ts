7cd4882adfb0124ff8b50853327f16d3ade6c481
Agry88 <rgok307085@gmail.com>
2024-01-12 18:27:56 +0800

    feat: add useTenementSellInfo hook.

--- src/hooks/new-tenement/useTenement.ts
+++ src/hooks/new-tenement/useTenement.ts
@@ -398,3 +398,152 @@ export function useTenementDevelopInfo(tenementId: string) {
     },
   };
 }
+
+type ITenementSellInfoType = ITenementBasedInfoType & {
+  tenement_status: "未成交" | "已成交" | "已成交下架" | "過戶完成下架";
+
+  total_rating: string;
+  main_building: string;
+  affiliated_building: string;
+  public_buliding: string;
+  unregistered_area: string;
+  management_magnification: string;
+  management_fee: string;
+  selling_price: string;
+  tenement_floor: string;
+
+  tenement_host_name: string;
+  tenement_host_telphone: string;
+  tenement_host_phone: string;
+  tenement_host_line: string;
+  tenement_host_remittance_bank: string;
+  tenement_host_remittance_account: string;
+  tenement_host_address: string;
+  tenement_host_birthday: string;
+  tenement_host_hobby: string;
+  tenement_host_remark: string;
+
+  buyer_order_date: string;
+  buyer_handout_date: string;
+  buyer_name: string;
+  buyer_id_images: string[];
+  buyer_phone: string;
+  buyer_jobtitle: string;
+  buyer_remark: string;
+};
+
+export function useTenementSellInfo(tenementId: string) {
+  const [sellInfo, setSellInfo] = useState<ITenementSellInfoType>({
+    tenement_address: "1234",
+    tenement_product_type: "套房",
+    tenement_type: "行銷追蹤",
+    tenement_face: "海景",
+    tenement_images: [
+      "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    ],
+    tenement_status: "已成交",
+    total_rating: "4.5",
+    main_building: "100",
+    affiliated_building: "50",
+    public_buliding: "30",
+    unregistered_area: "20",
+    management_magnification: "1.5",
+    management_fee: "3000",
+    selling_price: "500000",
+    tenement_floor: "7",
+
+    tenement_host_name: "John Doe",
+    tenement_host_telphone: "1234567890",
+    tenement_host_phone: "0987654321",
+    tenement_host_line: "john_doe",
+    tenement_host_remittance_bank: "Bank of America",
+    tenement_host_remittance_account: "9876543210",
+    tenement_host_address: "123 Main St, City, Country",
+    tenement_host_birthday: "1980-01-01",
+    tenement_host_hobby: "Reading",
+    tenement_host_remark: "No remarks",
+
+    buyer_order_date: "2022-01-01",
+    buyer_handout_date: "2022-02-01",
+    buyer_name: "Jane Doe",
+    buyer_id_images: ["image1.jpg", "image2.jpg"],
+    buyer_phone: "0987654321",
+    buyer_jobtitle: "Software Engineer",
+    buyer_remark: "No remarks",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleChange = (key: keyof ITenementRentInfoType, value: string) => {
+    setSellInfo((prev) => {
+      if (!prev) return prev;
+      return { ...prev, [key]: value };
+    });
+  };
+
+  const handleSave = () => {
+    // handle save data here
+    console.log(sellInfo);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    try {
+      // handle fetch data here based on tenementId
+      setSellInfo({
+        tenement_address: "1234",
+        tenement_product_type: "套房",
+        tenement_type: "行銷追蹤",
+        tenement_face: "海景",
+        tenement_images: [
+          "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        ],
+        tenement_status: "已成交",
+        total_rating: "4.5",
+        main_building: "100",
+        affiliated_building: "50",
+        public_buliding: "30",
+        unregistered_area: "20",
+        management_magnification: "1.5",
+        management_fee: "3000",
+        selling_price: "500000",
+        tenement_floor: "7",
+
+        tenement_host_name: "John Doe",
+        tenement_host_telphone: "1234567890",
+        tenement_host_phone: "0987654321",
+        tenement_host_line: "john_doe",
+        tenement_host_remittance_bank: "Bank of America",
+        tenement_host_remittance_account: "9876543210",
+        tenement_host_address: "123 Main St, City, Country",
+        tenement_host_birthday: "1980-01-01",
+        tenement_host_hobby: "Reading",
+        tenement_host_remark: "No remarks",
+
+        buyer_order_date: "2022-01-01",
+        buyer_handout_date: "2022-02-01",
+        buyer_name: "Jane Doe",
+        buyer_id_images: ["image1.jpg", "image2.jpg"],
+        buyer_phone: "0987654321",
+        buyer_jobtitle: "Software Engineer",
+        buyer_remark: "No remarks",
+      });
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  }, [tenementId]);
+
+  return {
+    states: {
+      sellInfo,
+      isLoading,
+      isError,
+    },
+    handlers: {
+      handleChange,
+      handleSave,
+    },
+  };
+}
