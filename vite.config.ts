6794da7c4d9631afc30c14d79205372e096cea74
jiweihong0 <jiweihong0@gmail.com>
2024-02-02 15:23:26 +0800

    try: for 2/3 version

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
-import {  defineConfig, loadEnv } from 'vite'
-import { viteMockServe } from 'vite-plugin-mock'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import { viteMockServe } from "vite-plugin-mock";
+import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig((command)=>{
-  const env = loadEnv(command.command, process.cwd(), '')
+export default defineConfig((command) => {
+  const env = loadEnv(command.command, process.cwd(), "");
   return {
     define: {
-      'process.env': env,
+      "process.env": env,
+    },
+    server: {
+      port: 3000,
+      open: true,
+      host: "0.0.0.0",
     },
     plugins: [
       react(),
       viteMockServe({
-        mockPath: './src/mock/',
-        localEnabled: command.command === 'serve',
-        prodEnabled: command.command !== 'serve' ,
+        mockPath: "./src/mock/",
+        localEnabled: command.command === "serve",
+        prodEnabled: command.command !== "serve",
         supportTs: true,
         watchFiles: true,
       }),
     ],
-  }
-})
+  };
+});
